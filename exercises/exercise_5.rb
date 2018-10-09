75ee175b7f3fc4fa537bb9c42eeff5007829745c
Patrick Chu <mingweipatrickc@gmail.com>
2018-10-09 17:33:30 +0000

    Finished exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,18 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+class Stores < ActiveRecord::Base
+
+  total_revenue = Stores.sum(:annual_revenue)
+
+  puts total_revenue
+  stores = Stores.all()
+
+  stores.each do |store|
+    average = store.annual_revenue/6
+    puts "average is #{average}"
+  end
+
+  puts stores_over_million = Stores.where("annual_revenue > 1000000").size
+
+end
